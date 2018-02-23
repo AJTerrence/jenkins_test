92cbdac207bb3ee964bde7588acd59eb0b770e2a
AJTerrence <zhuang@kalengo.com>
2018-02-23 15:48:07 +0800

    change api

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(async (ctx, next) => {
 })
 
 
-router.get('/api/user', async function(ctx){
+router.get('/api/key', async function(ctx){
 	ctx.status = 200;
 	ctx.body = {code:0,msg:'success'}
 })
